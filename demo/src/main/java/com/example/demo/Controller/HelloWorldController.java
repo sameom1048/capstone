cd23d1f550cf1ebbfd82534b97400e001ca6ea58
sameom1048 <samvision99@gmail.com>
2023-03-13 00:45:29 +0900

    first commit

--- demo/src/main/java/com/example/demo/Controller/HelloWorldController.java
+++ demo/src/main/java/com/example/demo/Controller/HelloWorldController.java
@@ -1,17 +1,26 @@
 package com.example.demo.Controller;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import ch.qos.logback.core.util.SystemInfo;
+import com.example.demo.Model.User;
+import com.example.demo.Sevice.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.lang.management.ManagementFactory;
 import java.lang.management.OperatingSystemMXBean;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 @CrossOrigin
 @RestController
 public class HelloWorldController {
+
+    @Autowired //Bean으로 등록된 클래스들을 스프링을 시작할 때 (서버를 켤 때)자동으로 주입
+    private UserService userService;
 
     @GetMapping("/api/data")
     public Map<String, String> getCPUInfo() {
@@ -27,5 +36,49 @@ public class HelloWorldController {
 
         return data;
     }
+
+//    @GetMapping("/cpu")
+//    public String getCpuInfo() {
+//        OperatingSystemMXBean osBean = (OperatingSystemMXBean) ManagementFactory.getOperatingSystemMXBean();
+//        String cpuName = osBean.getProcessor().getProcessorIdentifier().getName();
+//        int cpuCount = osBean.getAvailableProcessors();
+//        return "CPU Name: " + cpuName + ", CPU Count: " + cpuCount;
+//    }
+
+    @GetMapping("/api/userlist")
+    public ResponseEntity<List<User>> findAll() {
+        return new ResponseEntity<>(userService.findAll(), HttpStatus.OK);
+    }
+    @GetMapping("/api/signup/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id) {
+        Optional<User> optionalUser = userService.findById(id);
+        if (optionalUser.isPresent()) {
+            return new ResponseEntity<>(optionalUser.get(), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+    @PostMapping("/api/signup")
+    public ResponseEntity<User> save(@RequestBody User user) {
+        System.out.println("successfully response!");
+        return new ResponseEntity<>(userService.save(user), HttpStatus.CREATED);
+    }
+    @DeleteMapping("/api/signup/{id}")
+    public ResponseEntity<Void> deleteById(@PathVariable Long id) {
+        userService.deleteById(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+//    public ResponseEntity<?> registerUser(@RequestBody SignupRequest signupRequest) {
+//        if (userService.existsByEmail(signupRequest.getEmail())) {
+//            return new ResponseEntity<>(new ApiResponse(false, "Email address already in use!"), HttpStatus.BAD_REQUEST);
+//        }
+//
+//        User user = new User(signupRequest.getEmail(), signupRequest.getPassword());
+//
+//        userService.save(user);
+//
+//        return ResponseEntity.ok(new ApiResponse(true, "User registered successfully"));
+//    }
 }
 
