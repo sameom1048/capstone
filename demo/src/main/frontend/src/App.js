2cd2f94b172ef1aa13f38248d8a600ad6f6ade08
sameom1048 <samvision99@gmail.com>
2023-03-13 00:45:29 +0900

    first commit

--- demo/src/main/frontend/src/App.js
+++ demo/src/main/frontend/src/App.js
@@ -1,57 +1,75 @@
-import React, { useEffect, useState } from "react";
-import axios from "axios";
+import "./App.css";
+import 'bootstrap/dist/css/bootstrap.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { useState } from "react";
+import { TopNavigationBar } from "./components/header/topNavigationBar";
+import Home from "./pages/home";
+import Product from "./pages/product";
+import Basket from "./pages/basket";
+import TopButton from "./components/topButton/topButton";
+import CategoryBar from "./components/category/categoryBar";
+import { OrderComplete } from "./components/cart/orderComplete";
+import { CategoryMove } from "./components/category/categoryMove";
 
 function App() {
-    const [data, setData] = useState(null);
-    const [cpuInfo, setCpuInfo] = useState('');
-    const [gpuInfo, setGpuInfo] = useState('')
+  const [products, setProducts] = useState([]);
+  const [cart, setCart] = useState([]);
 
-    useEffect(() => {
-        // CPU 정보 가져오기
-        const cpu = navigator.hardwareConcurrency;
-        setCpuInfo(`CPU 모델명: ${cpu}`);
+  const convertPrice = (price) => {
+    return price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  };
 
-        // WebGL을 사용하여 GPU 정보 가져오기
-        const canvas = document.createElement('canvas');
-        const gl = canvas.getContext('webgl') || canvas.getContext('experimental-webgl');
-        if (gl) {
-            const info = gl.getExtension('WEBGL_debug_renderer_info');
-            if (info) {
-                const gpu = gl.getParameter(info.UNMASKED_RENDERER_WEBGL);
-                setGpuInfo(`GPU 모델명: ${gpu}`);
-            } else {
-                setGpuInfo('GPU 정보를 가져올 수 없습니다.');
-            }
-        } else {
-            setGpuInfo('WebGL을 지원하지 않는 브라우저입니다.');
-        }
-    }, []);
-
-    const handleClick = async () => {
-        try {
-            const response = await axios.get("http://localhost:12000/api/data");
-            console.log("받은 데이터:", response.data);
-            setData(response.data);
-        } catch (error) {
-            console.error(error);
-            // TODO: 오류 처리 로직을 추가
-        }
-    };
-
-    return (
-        <div>
-            <button onClick={handleClick}>Send Request to Spring Boot Server</button>
-            {data && (
-                <ul>
-                    {data.map((item) => (
-                        <li key={item.id}>{item.name}</li>
-                    ))}
-                </ul>
-            )}
-            {cpuInfo !== null && <p>CPU 정보: {cpuInfo}</p>}
-            {gpuInfo !== null && <p>GPU 정보: {gpuInfo}</p>}
-        </div>
-    );
+  return (
+    <BrowserRouter>
+      <TopNavigationBar cart={cart} />
+      {/* <CategoryBar></CategoryBar> */}
+      <TopButton></TopButton>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <Home
+              convertPrice={convertPrice}
+              products={products}
+              setProducts={setProducts}
+            />
+          }
+        />
+        <Route
+          path="/product/:id"
+          element={
+            <Product
+              convertPrice={convertPrice}
+              cart={cart}
+              setCart={setCart}
+            />
+          }
+        />
+        <Route
+          path="/cart"
+          element={
+            <Basket cart={cart} setCart={setCart} convertPrice={convertPrice} />
+          }
+        />
+        <Route
+          path="/cart/orderComplete"
+          element={
+            <OrderComplete />
+          }
+        />
+        <Route
+          path="/category/:n"
+          element={
+            <CategoryMove
+              convertPrice={convertPrice}
+              products={products}
+              setProducts={setProducts}
+            />
+          }
+        />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
